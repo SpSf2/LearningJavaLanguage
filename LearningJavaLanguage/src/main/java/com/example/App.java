b4fa119cc9d7dbd3eb58e7138e5015c5776aea6e
SpSf2 <kiworks.duglas@gmail.com>
2026-04-15 14:27:31 +0200

    Pattern Maching con instanceof explicado

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -5,19 +5,21 @@ import java.time.Month;
  
 public class App {
 	
-	static String printObject(Object object) {
+	static String printObject(Object obj) {
 		
 		/*Recibe como parametro un object y en dependencia del tipo, imprimirá el salario si se trata de
 		 * un empleado o el total de asignaturas si se tratase de un estudiante*/
 		
-		if (object instanceof Empleado) {
+		/*  Utilizando los beneficios de Pattern Maching  */
+		if (obj instanceof Empleado empleado) {
 			
-			Empleado empleado = (Empleado) object;
+			// Empleado empleado = (Empleado) object;
+			
 			
 			return "Salario del empleado: " + empleado.getSalario();
-		} else if (object instanceof Estudiante)  {
+		} else if (obj instanceof Estudiante estudiante)  {
 			
-			Estudiante estudiante = (Estudiante) object;
+			// Estudiante estudiante = (Estudiante) object;
 			
 			return "Total de Asignaturas del Estudiante: " + estudiante.getTotalAsignaturas();
 			
